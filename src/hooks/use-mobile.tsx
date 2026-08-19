57e4caa2f5d7808ede1ae5917374a6968b579a90
sameer2880 <shaiksameer41517@gmail.com>
2026-08-20 00:28:50 +0530

    Your commit message

--- src/hooks/use-mobile.tsx
+++ src/hooks/use-mobile.tsx
@@ -6,13 +6,27 @@ export function useIsMobile() {
   const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined);
 
   React.useEffect(() => {
+    if (typeof window.matchMedia !== "function") {
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+      return;
+    }
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`);
     const onChange = () => {
       setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
     };
-    mql.addEventListener("change", onChange);
+    if (typeof mql.addEventListener === "function") {
+      mql.addEventListener("change", onChange);
+    } else {
+      mql.addListener(onChange);
+    }
     setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
-    return () => mql.removeEventListener("change", onChange);
+    return () => {
+      if (typeof mql.removeEventListener === "function") {
+        mql.removeEventListener("change", onChange);
+      } else {
+        mql.removeListener(onChange);
+      }
+    };
   }, []);
 
   return !!isMobile;

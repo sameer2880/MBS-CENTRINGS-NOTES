30735a1e78ac9ad05da7161acef341849ad696b1
sameer2880 <shaiksameer41517@gmail.com>
2026-08-29 20:11:36 +0530

    Your commit message

--- src/lib/gate.tsx
+++ src/lib/gate.tsx
@@ -252,7 +252,7 @@ export function Gate({ children }: { children: ReactNode }) {
       }
       const password = p.trim();
       if (password !== userRecord.phone.trim()) {
-        setErr("Incorrect mobile number");
+        setErr("Incorrect Credentials, Contact admin to get login credentials");
         return;
       }
       const role = getRole(userRecord.notes);
